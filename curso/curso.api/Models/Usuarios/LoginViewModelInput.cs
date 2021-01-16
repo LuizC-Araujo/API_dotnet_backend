968691e6e71b2c33d5f174bfd21cbbc3bcf900b1
Luiz Araujo <arauldesign@gmail.com>
2021-01-16 17:53:58 -0300

    adicionando rotas usuario

---
+++ curso/curso.api/Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Models.Usuarios
+{
+    public class LoginViewModelInput
+    {
+        public string login { get; set; }
+        public string senha { get; set; }
+    }
+}
